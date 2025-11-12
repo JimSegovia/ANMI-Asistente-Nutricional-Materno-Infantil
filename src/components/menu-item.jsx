db46bb1d5d13c0d045d7f95b5273052e55766c1b
Esther <esther.ticllav@gmail.com>
2025-11-11 20:05:24 -0500

    Modify menu-item link chatbot page

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MenuItem({ icon, title, description, type }) {
-  const mostrarSeccion = () => {
-    alert(
-      `Sección "${title.toUpperCase()}" en desarrollo.\n\nEsta funcionalidad estará disponible en las próximas semanas del proyecto.`
+  const contenidoDeLaTarjeta = (
+    <div> 
+      <img src={icon} alt={title}/>
+      <h3 className="menu-item-title">{title}</h3>
+      <p className="menu-item-description">{description}</p>
+    </div>
+  );
+
+  if (title === "Chatbot ANMI") {
+    return (
+      <Link to="/chatbot">
+        {contenidoDeLaTarjeta}
+      </Link>
     );
-  };
+  } else {
+    return (
+      <div>
+        {contenidoDeLaTarjeta}
+      </div>
+    );
+  }
 }
