ff30187bd3e7d0fdf301584c92d7c2dbe0e9847b
Jim B. Segovia <jim.b.segovia@gmail.com>
2025-11-11 21:10:09 -0500

    se cambio la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from "./components/header";
 import MenuGrid from "./components/menu-grid";
 import Footer from "./components/footer";
-import ChatbotPage from "./pages/ChatbotPage";
+import ChatbotPage from "./pages/chatbot-page";
 
 import './App.css'
 
